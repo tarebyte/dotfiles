6aaa69a0d28447715b065bc3a3baf320d7bb736f
Mark Tareshawty <mark@marktareshawty.com>
2016-01-06 11:40:30 -0500

    can't do that

--- iterm/all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo 'Overwriting iterm preferences'
-rm -f ~/Library/Preferences/com.googlecode.iterm2.plist
-ln -s $HOME/.dotfiles/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
