35db1e79861ef880f58c3ae2fa88706979e4ab5c
Mark Tareshawty <mark@marktareshawty.com>
2021-07-24 17:25:36 -0400

    Remove vendor fish plugins from fisher

--- config/fish/functions/_tide_decolor.fish
+++
@@ -1,3 +0,0 @@
-function _tide_decolor -a text
-    printf '%s' "$text" | string replace -a (set_color normal) '' | string replace -ar '\e\[[0-9;]*m' ''
-end
--- config/fish/functions/_tide_item_git_prompt.fish
+++
@@ -1,5 +0,0 @@
-function _tide_item_git_prompt
-    if test -n "$git_dir"
-        fish_git_prompt '%s '
-    end
-end
