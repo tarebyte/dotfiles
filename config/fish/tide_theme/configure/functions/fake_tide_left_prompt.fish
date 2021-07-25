ba3b8ff335a45ccbd14715981f57b5a3d6977f52
Mark Tareshawty <mark@marktareshawty.com>
2021-07-25 13:22:10 -0400

    Remove tide_theme directory

--- config/fish/tide_theme/configure/functions/fake_tide_left_prompt.fish
+++
@@ -1,8 +0,0 @@
-function _fake_tide_left_prompt
-    for item in _fake_tide_item_{$fake_tide_left_prompt_items}
-        set -l itemOutput ($item)
-        printf '%b' "$itemOutput"
-
-        set_color normal
-    end
-end
--- config/fish/tide_theme/configure/prompt_items/_fake_tide_item_cmd_duration.fish
+++
@@ -1,4 +0,0 @@
-function _fake_tide_item_cmd_duration
-    set_color $tide_cmd_duration_color
-    printf '%s ' '5s'
-end
