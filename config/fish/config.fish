dbcc476f7fc7c533640ad551bb5545a32b828161
Mark Tareshawty <mark@marktareshawty.com>
2021-08-24 09:13:23 -0400

    Use fish_add_path instead of set

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -22,7 +22,7 @@ else if test -d "/home/linuxbrew/.linuxbrew"
 	set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
 	set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
 	set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
-	set -q PATH; or set PATH ''; set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
+	set -q PATH; or set PATH ''; fish_add_path -gP "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin"
 	set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
 	set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
 end
