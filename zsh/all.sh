dbd67bc6592d527d8af8f96d34c4266d8fb3e115
Patrick Donovan <patrick.donovan@shopify.com>
2022-01-05 11:39:28 -0600

    automate autojump installation for osx

--- zsh/all.sh
+++ zsh/all.sh
@@ -7,6 +7,15 @@ if [[ $ZSH_NAME != "zsh" ]]; then
   chsh -s /bin/zsh
 fi
 
-# TODO: this path is wrong. I think this has been broken
-# for a while. does anything use colors?
+if test $PLATFORM == "osx"; then
+  result=`brew --prefix autojump`
+  if test -e "$result" ; then
+    echo "autojump installed"
+  else
+    echo "install autojump - used by oh-my-zsh - with brew"
+    brew install autojump
+  fi
+fi
+
+# do I really want this, even?
 # sh $HOME/.dotfiles/colors/base16-shell/base16-ocean.dark.sh
