0f73523c2d83dbab5d8b989c7cd4e6117b204466
Mark Tareshawty <mark@marktareshawty.com>
2022-01-31 07:19:06 -0500

    Update sections

--- .config/nvim/lua/user/plugins/lualine.lua
+++ .config/nvim/lua/user/plugins/lualine.lua
@@ -3,6 +3,10 @@ if not status_ok then
 	return
 end
 
+vim.cmd([[
+	highlight LualineWarning guifg=#EBCB8B guibg=#4F5B66
+]])
+
 lualine.setup({
 	extensions = { "fugitive", "fzf" },
 	options = {
@@ -14,17 +18,26 @@ lualine.setup({
 	sections = {
 		lualine_b = {
 			{ "branch", icon = "" },
-			{ "diff", symbols = { added = ' ', modified = ' ', removed = ' ' } },
+			{ "diff", symbols = { added = " ", modified = " ", removed = " " } },
+			{
+				"diagnostics",
+				sources = { "ale" },
+				diagnostics_color = {
+					warn = "LualineWarning",
+				},
+				symbols = { error = " ", warn = " ", info = " ", hint = " " },
+			},
 		},
 		lualine_c = {
 			{ "filename", path = 1 },
 		},
+		lualine_x = {
+			"encoding",
+			"fileformat",
+			{ "filetype", colored = false, icon_only = true },
+		},
 	},
 	inactive_sections = {
-		lualine_b = {
-			{ "branch", icon = "" },
-			{ "diff", symbols = { added = ' ', modified = ' ', removed = ' ' } },
-		},
 		lualine_c = {
 			{ "filename", path = 1 },
 		},
