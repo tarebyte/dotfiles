582ac65394c81dc781203bdce24b4a894d725aff
Mark Tareshawty <mark@marktareshawty.com>
2022-02-01 06:52:53 -0500

    Load plugins before trying to set the color scheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 require("user.options")
 require("user.keymaps")
-require("user.colorscheme")
 require("user.plugins")
+require("user.colorscheme")
 require("user.autocommands")
 
 -- Cross cutting plugin configuruations
