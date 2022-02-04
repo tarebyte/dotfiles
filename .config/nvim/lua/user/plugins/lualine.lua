eb18975d1e61a8e39df1a3af6b81fe9872574e19
Mark Tareshawty <mark@marktareshawty.com>
2022-02-03 22:36:51 -0500

    Minor lualine changes

--- .config/nvim/lua/user/plugins/lualine.lua
+++ .config/nvim/lua/user/plugins/lualine.lua
@@ -3,6 +3,18 @@ if not status_ok then
 	return
 end
 
+-- https://github.com/LunarVim/LunarVim/blob/41b3f63c37ce2f79defc22a2cbcd347281a808a5/lua/lvim/core/lualine/components.lua#L4-L13
+local function diff_source()
+	local gitsigns = vim.b.gitsigns_status_dict
+	if gitsigns then
+		return {
+			added = gitsigns.added,
+			modified = gitsigns.changed,
+			removed = gitsigns.removed,
+		}
+	end
+end
+
 vim.cmd([[
 	highlight LualineWarning guifg=#EBCB8B guibg=#4F5B66
 ]])
@@ -16,9 +28,18 @@ lualine.setup({
 		theme = "base16_ocean",
 	},
 	sections = {
+		lualine_a = {
+			{
+				"mode",
+				fmt = function(str)
+					return str:sub(1, 1)
+				end,
+				padding = 1,
+			},
+		},
 		lualine_b = {
-			{ "branch", icon = "" },
-			{ "diff", symbols = { added = " ", modified = " ", removed = " " } },
+			{ "branch", "b:gitsigns_head", icon = "" },
+			{ "diff", source = diff_source(), symbols = { added = " ", modified = " ", removed = " " } },
 			{
 				"diagnostics",
 				sources = { "ale" },
@@ -34,7 +55,7 @@ lualine.setup({
 		lualine_x = {
 			"encoding",
 			"fileformat",
-			{ "filetype", colored = false, icon_only = true },
+			{ "filetype", colored = false },
 		},
 	},
 	inactive_sections = {
