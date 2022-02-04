f98fc7fcf43c91d96a048ab68775db111656a4b5
Mark Tareshawty <mark@marktareshawty.com>
2022-02-03 22:35:09 -0500

    Lable colors and add color for command mode

--- .config/nvim/lua/lualine/themes/base16_ocean.lua
+++ .config/nvim/lua/lualine/themes/base16_ocean.lua
@@ -1,21 +1,20 @@
--- stylelua: ignore
 local colors = {
-	color00 = "#2b303b",
-	color01 = "#343d46",
-	color02 = "#4f5b66",
-	color03 = "#65737e",
-	color04 = "#a7adba",
-	color05 = "#c0c5ce",
-	color06 = "#dfe1e8",
-	color07 = "#eff1f5",
-	color08 = "#bf616a",
-	color09 = "#d08770",
-	color0A = "#ebcb8b",
-	color0B = "#a3be8c",
-	color0C = "#96b5b4",
-	color0D = "#8fa1b3",
-	color0E = "#b48ead",
-	color0F = "#ab7967",
+	color00 = "#2b303b", -- Background
+	color01 = "#343d46", -- Almost background
+	color02 = "#4f5b66", -- Darkest gray
+	color03 = "#65737e", -- Bright black
+	color04 = "#a7adba", -- Light gray
+	color05 = "#c0c5ce", -- White
+	color06 = "#dfe1e8", -- Darker white
+	color07 = "#eff1f5", -- Bright White
+	color08 = "#bf616a", -- Red
+	color09 = "#d08770", -- Bright orange
+	color0A = "#ebcb8b", -- Yellow
+	color0B = "#a3be8c", -- Green
+	color0C = "#96b5b4", -- Cyan
+	color0D = "#8fa1b3", -- Blue
+	color0E = "#b48ead", -- Magenta
+	color0F = "#ab7967", -- Darker Orange
 }
 
 return {
@@ -36,6 +35,11 @@ return {
 	},
 	visual = {
 		a = { fg = colors.color01, bg = colors.color0E, gui = "bold" },
+		b = { fg = colors.color06, bg = colors.color02 },
+		c = { fg = colors.color09, bg = colors.color01 },
+	},
+	command = {
+		a = { fg = colors.color01, bg = colors.color0C, gui = "bold" },
 		b = { fg = colors.color06, bg = colors.color02 },
 		c = { fg = colors.color09, bg = colors.color01 },
 	},
