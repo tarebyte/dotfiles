5a244f631846b42306213bb43397ff41d98fbc00
Mark Tareshawty <mark@marktareshawty.com>
2022-04-18 08:38:46 -0400

    Fix https://github.com/hrsh7th/nvim-cmp/issues/231#issuecomment-1098175017

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -28,18 +28,13 @@ cmp.setup({
 	formatting = {
 		format = lspkind.cmp_format({ preset = "codicons" }),
 	},
-	mapping = {
-		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-		["<C-e>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-y>"] = cmp.config.disable,
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-
-		["<Tab>"] = cmp.mapping(function(fallback)
+		["<Tab>"] = function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expandable() then
@@ -51,9 +46,8 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
+		end,
+		["<S-Tab>"] = function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
@@ -61,8 +55,8 @@ cmp.setup({
 			else
 				fallback()
 			end
-		end, { "i", "s" }),
-	},
+		end
+	}),
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "nvim_lua" },
