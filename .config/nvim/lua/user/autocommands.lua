95265d2a23da5e3156a0065d7850a005be1f9b4b
Mark Tareshawty <mark@marktareshawty.com>
2022-04-28 22:07:12 -0400

    Use new autocmd syntax and move :Search to commands file

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -1,11 +1,28 @@
-vim.cmd('au BufReadPost * if line("\'\\"") > 1 && line("\'\\"") < line("$") | exe "normal! g\'\\"" | endif')
+-- Lua conversion of restore-cursor last-position-jump
+--
+-- autocmd BufRead * autocmd FileType <buffer> ++once
+--   \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
+--
+-- https://www.reddit.com/r/neovim/comments/ucgxmj/comment/i6coai3/?utm_source=share&utm_medium=web2x&context=3
 
-vim.cmd([[
-	" Create a search command that uses Ripgrep and offers previews
-	command! -bang -complete=file -nargs=* Search
-	\ call fzf#vim#grep(
-	\   'rg --smart-case --vimgrep --no-heading --color=always '.<q-args>, 1,
-	\   <bang>0 ? fzf#vim#with_preview('up:60%')
-	\           : fzf#vim#with_preview('right:50%:hidden', '?'),
-	\   <bang>0)
-]])
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+	pattern = { "*" },
+	callback = function()
+		local ft = vim.opt_local.filetype:get()
+
+		-- don't apply to git messages
+		if ft:match("commit") or ft:match("rebase") then
+			return
+		end
+
+		-- get position of last saved edit
+		local mark_position = vim.api.nvim_buf_get_mark(0, '"')
+
+		local row = mark_position[1]
+		local col = mark_position[2]
+
+		if (row > 1) and (row <= vim.api.nvim_buf_line_count(0)) then
+			vim.api.nvim_win_set_cursor(0, { row, col })
+		end
+	end,
+})
