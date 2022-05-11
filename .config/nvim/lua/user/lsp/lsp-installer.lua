e537142cffee27f2735c75a54a8e2e095b480fec
Mark Tareshawty <tarebyte@github.com>
2022-05-11 15:50:10 -0400

    Fix LSP installer deprecations
    https://github.com/williamboman/nvim-lsp-installer/discussions/636

--- .config/nvim/lua/user/lsp/lsp-installer.lua
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -3,20 +3,12 @@ if not ok then
 	return
 end
 
--- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
--- or if the server is already installed).
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
+lsp_installer.setup({
+	capabilities = require("user.lsp.handlers").capabilities,
+})
 
-	if server.name == "sumneko_lua" then
-		local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	end
+local lspconfig = require("lspconfig")
 
-	-- This setup() function will take the provided server configuration and decorate it with the necessary properties
-	-- before passing it onwards to lspconfig.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
+lspconfig.sumneko_lua.setup({
+	settings = require("user.lsp.settings.sumneko_lua")
+})
--- .config/nvim/lua/user/lsp/settings/sumneko_lua.lua
+++ .config/nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -5,24 +5,20 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 return {
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-				path = runtime_path,
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-			telemetry = {
-				enable = false,
-			},
+	Lua = {
+		runtime = {
+			version = "LuaJIT",
+			path = runtime_path,
+		},
+		diagnostics = {
+			globals = { "vim" },
+		},
+		workspace = {
+			-- Make the server aware of Neovim runtime files
+			library = vim.api.nvim_get_runtime_file("", true),
+		},
+		telemetry = {
+			enable = false,
 		},
 	},
 }
