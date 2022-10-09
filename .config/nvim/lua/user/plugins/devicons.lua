fcdf5bf71aeb81fb451b380bc1b1068322488bc6
Mark Tareshawty <mark@marktareshawty.com>
2022-10-09 15:35:02 -0400

    Fix icons and colors

--- .config/nvim/lua/user/plugins/devicons.lua
+++ .config/nvim/lua/user/plugins/devicons.lua
@@ -5,15 +5,33 @@ end
 
 devicons.setup({
 	override = {
+		["config.ru"] = {
+			icon = "",
+			color = "#BF616A",
+			cterm_color = "52",
+			name = "ConfigRu",
+		},
+		erb = {
+			icon = "",
+			color = "#BF616A",
+			cterm_color = "52",
+			name = "Erb",
+		},
 		fish = {
 			icon = "",
 			color = "#4d5a5e",
 			cterm_color = "59",
 			name = "Fish",
 		},
+		rake = {
+			icon = "",
+			color = "#BF616A",
+			cterm_color = "52",
+			name = "Rake",
+		},
 		rb = {
-			icon = " ",
-			color = "#701516",
+			icon = "",
+			color = "#BF616A",
 			cterm_color = "52",
 			name = "Rb",
 		},
