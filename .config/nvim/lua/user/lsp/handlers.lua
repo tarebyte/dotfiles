a61095fff71a1bf5ad790c3ee430b67466d21003
Mark Tareshawty <tarebyte@github.com>
2022-10-17 17:44:52 -0400

    Use new default_capabilities() method

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -63,13 +63,11 @@ M.on_attach = function(_, bufnr)
 	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
 	return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
