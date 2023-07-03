01a864398ed8d8d69f48a5395d658d69ca3b4db7
Mark Tareshawty <mark@marktareshawty.com>
2023-07-03 14:12:23 -0400

    Add icons.lua

---
+++ .config/nvim/lua/user/utils/icons.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+-- https://github.com/onsails/lspkind.nvim/blob/57610d5ab560c073c465d6faf0c19f200cb67e6e/lua/lspkind/init.lua#L34-L60
+-- Plus Copilot
+M.codicons = {
+	Text = "",
+	Method = "",
+	Function = "",
+	Constructor = "",
+	Field = "",
+	Variable = "",
+	Class = "",
+	Interface = "",
+	Module = "",
+	Property = "",
+	Unit = "",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "",
+	Event = "",
+	Operator = "",
+	TypeParameter = "",
+	Namespace = "",
+	Package = "",
+	String = "",
+	Number = "",
+	Boolean = "",
+	Array = "",
+	Object = "",
+	Key = "",
+	Null = "",
+	Copilot = "",
+}
+
+return M
