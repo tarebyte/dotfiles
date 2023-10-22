c87d631ff5a3d038e710b28ed6e07cb15595d68b
Mark Tareshawty <mark@marktareshawty.com>
2023-10-22 13:49:15 -0400

    Remove custom icons for gitsigns

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -6,12 +6,7 @@ return {
   },
   {
     "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
     opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-      },
       numhl = true,
       current_line_blame = true,
       current_line_blame_opts = {
