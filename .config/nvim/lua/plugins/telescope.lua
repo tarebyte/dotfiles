97f83268a693db7cda14403a2274e5bf8f34b7a7
Mark Tareshawty <tarebyte@github.com>
2024-06-11 09:50:02 -0400

    s/telescope/pick

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -24,8 +24,8 @@ return {
       -- https://github.com/LazyVim/LazyVim/blob/6f9adbd4fba4132bd4f12404bd2b90c4a28ff136/lua/lazyvim/plugins/editor.lua
       { "<leader><space>", false },
 
-      { "<C-p>", Util.telescope("files"), desc = "Find Files (root dir)" },
-      { "<leader>t", Util.telescope("files"), desc = "Find Files (root dir)" },
+      { "<C-p>", Util.pick("files"), desc = "Find Files (root dir)" },
+      { "<leader>t", Util.pick("files"), desc = "Find Files (root dir)" },
       { "<C-]>", "<cmd>lua require('telescope').extensions.ctags_plus.jump_to_tag()<cr>", desc = "CTags" },
     },
   },
