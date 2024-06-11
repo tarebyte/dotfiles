8039587f95f548d8fe636a51eb05262147f53a3b
Mark Tareshawty <mark@marktareshawty.com>
2024-06-10 21:53:22 -0400

    Remove deprecated method call

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -11,7 +11,7 @@ return {
 
       -- https://github.com/zbirenbaum/copilot-cmp#tab-completion-configuration-highly-recommended
       local has_words_before = function()
-        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        if vim.api.nvim_get_option_value("buftype", {}) == "prompt" then
           return false
         end
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
