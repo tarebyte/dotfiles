ac53353b01bf674c78bbed497a3990666801524f
Mark Tareshawty <tarebyte@github.com>
2025-11-05 11:42:46 -0500

    Fix mini.* disable owner

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
+  { "nvim-mini/mini.surround", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
