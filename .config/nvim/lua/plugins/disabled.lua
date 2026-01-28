f8e50a510b668d32485521b65921f01640a589d5
Mark Tareshawty <mark@marktareshawty.com>
2026-01-28 09:38:30 -0500

    Disable mason

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
+  { "mason-org/mason.nvim", enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
 }
